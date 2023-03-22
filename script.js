180b2c21d4dbab64697070b3eb3d10ba92625be6
Nikhil Kumar <85066913+nkshrivas@users.noreply.github.com>
2023-03-22 11:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+let start=0;
+let end = str.length-1;
 
+while(start<end){
+	if(str.textAt(start)!=str.textAt(end)){
+		
+	}
+	start+=1;
+	end+=1;
+}
+	if(start==end){
+		
+	}
 }
 module.exports = palindrome
